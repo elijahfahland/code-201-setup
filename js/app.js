b24319afba6d4095c7ddf8a0ac3ddaea922fe92c
elijah <elijah.fahland@gmail.com>
2018-08-05 13:31:30 -0700

    Finished HTML and CSS, halfway through JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,56 @@
-console.log('js is linked');
+'use strict';
+
+
+alert('Please type Yes, Y, No or N to answer the following questions');
+
+
+var answerFirst = prompt('Elijahs favorite car is the Toyota Supra?').toLowerCase();
+
+if (answerFirst === 'yes' || answerFirst === 'y' || answerFirst === 'no' || answerFirst === 'n'){
+  console.log('User replied to the first question with: ' + answerFirst);
+} else if (answerFirst !== 'yes' || answerFirst !== 'y' || answerFirst !== 'no' || answerFirst !== 'n'){
+  alert('Please use Yes, Y, No or N to answer the rest of the questions');
+}
+if (answerFirst === 'yes' || answerFirst === 'y'){
+  alert('Unfortunatley that was incorrect. Better luck next time!');
+}
+if (answerFirst === 'no' || answerFirst === 'n'){
+  alert('Great job! His favorite is the RX-7 of course!');
+}
+
+
+var answerSecond = prompt('Elijahs favorite mountain bike is the Santa Cruz Nomad').toLowerCase();
+
+if (answerSecond === 'yes' || answerSecond === 'y' || answerSecond === 'no' || answerSecond === 'n'){
+  console.log('User replied to the first question with: ' + answerSecond);
+} else if (answerSecond !== 'yes' || answerSecond !== 'y' || answerSecond !== 'no' || answerSecond !== 'n') {
+  alert('Please use Yes, Y, No or N to answer the rest of the questions');
+}
+if (answerSecond === 'yes' || answerSecond === 'y'){
+  alert('Great job! His favorite is the Nomad!');
+}
+if (answerSecond === 'no' || answerSecond === 'n'){
+  alert('Unfortunatley that was incorrect. Better luck next time!');
+}
+
+
+var answerThird = prompt('Elijahs favorite candy is lifesavers Wint-O-Green mints').toLowerCase();
+
+if (answerThird === 'yes' || answerThird === 'y' || answerThird === 'no' || answerThird === 'n'){
+  console.log('User replied to the first question with: ' + answerThird);
+} else if (answerThird !== 'yes' || answerThird !== 'y' || answerThird !== 'no' || answerThird !== 'n') {
+  alert('Please use Yes, Y, No or N to answer the rest of the questions');
+}
+if (answerThird === 'yes' || answerThird === 'y'){
+  alert('Great job! His favorite Wint-o-Green!');
+}
+if (answerThird === 'no' || answerThird === 'n'){
+  alert('Unfortunatley that was incorrect. Better luck next time!');
+}
+
+
+var answerFourth = prompt('Elijahs favorite City is Portland Oregon').toLowerCase;
+
+
+
+
